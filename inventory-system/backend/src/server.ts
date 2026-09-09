66891c696494b06689a994d98d6e50666012ee4f
Gabriel Livinalli <131381519+GLivinalli@users.noreply.github.com>
2026-09-09 20:33:54 -0300

    Update server.ts

--- inventory-system/backend/src/server.ts
+++ inventory-system/backend/src/server.ts
@@ -2,5 +2,5 @@ import { app } from "@/app";
 import { env } from "@/config/env";
 
 app.listen(env.PORT, () => {
-  console.log(`API rodando em http://localhost:${env.PORT} (${env.NODE_ENV})`);
+  console.log(`API rodando na porta ${env.PORT} (ambiente: ${env.NODE_ENV})`);
 });
